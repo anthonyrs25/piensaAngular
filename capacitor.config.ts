f23f397a04a2d7c3bb966d307e44d9d57bb74251
Anthony <afrodriguez@sudamericano.edu.ec>
2026-02-19 09:55:11 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'piensaAngular',
+  webDir: 'www'
+};
+
+export default config;
