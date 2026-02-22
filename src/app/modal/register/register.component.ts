2c804b13066be1cc3601f183b502b355ebeb0703
Anthony <afrodriguez@sudamericano.edu.ec>
2026-02-22 12:37:24 -0500

    vercel json

--- src/app/modal/register/register.component.ts
+++ src/app/modal/register/register.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
+  standalone: true,
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-  standalone: true,
 })
-export class RegisterComponent {
-
-}
+export class RegisterComponent {}
